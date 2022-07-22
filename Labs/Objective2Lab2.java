5be6b838f08a9abb16615c78dd2ee6501482d2de
Vanessa <nessi@Vanessas-MacBook-Pro.local>
2022-07-22 10:34:28 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String [] args) {
+    String name = "Vanessa Torres";
+    int age = 27;
+    String state = "Colorado";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
