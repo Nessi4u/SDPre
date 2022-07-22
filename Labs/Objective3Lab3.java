45bdbeada8706998c09a355fd3e14be1eab50ed3
Vanessa <nessi@Vanessas-MacBook-Pro.local>
2022-07-22 12:25:34 -0600

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,11 @@
+public class Objective3Lab3 {
+  public static void main(String [] args) {
+    int first, second, third, total;
+    first = (int)(Math.random()*6) +1;
+    second = (int)(Math.random()*6) +1;
+    third = (int)(Math.random()*6) +1;
+    total = first + second + third;
+    System.out.println(first + " + " + second + " + " + third + " = " + total);
+
+  }
+}
