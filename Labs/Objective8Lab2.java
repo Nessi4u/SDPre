7ca242f1ba70f67ecc6ab3e8784d73eb49979c4c
Vanessa <nessi@Vanessas-MacBook-Pro.local>
2022-08-03 09:43:48 -0600

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,9 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sumTotal = 0;
+    for(int i=1; i<=20; ++i){
+      sumTotal += i;
+    }
+    System.out.println(sumTotal);
+  }
+}
